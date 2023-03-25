5b1f8795f584830c928fcbef9493fd0533fe95b1
levko <levko@DESKTOP-6DC4QGL>
2023-03-25 22:20:25 +0300

    Добавьте файлы проекта.

---
+++ калькулятор/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace калькулятор
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double firstValue, secondValue;
+			string action;
+
+			Console.WriteLine("Введите число 1:");
+			firstValue = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Введите число 2:");
+			secondValue = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+
+			action = Console.ReadLine();
+
+			switch(action)
+			{
+				case "+":
+                    Console.WriteLine(firstValue + secondValue);
+                    break;
+
+			    case "-":
+                    Console.WriteLine(firstValue - secondValue);
+					break;
+
+				case "*":
+					Console.WriteLine(firstValue * secondValue);
+					break;
+
+			    case "/":
+					if(secondValue == 0)
+					{
+                        Console.WriteLine(0);
+                    }
+					else
+					{
+                        Console.WriteLine(firstValue / secondValue);
+                    }
+					break;
+
+				default: Console.WriteLine("Неизвестная команда!");
+					break;
+			}
+			Console.ReadKey();
+		}
+	}
+}
